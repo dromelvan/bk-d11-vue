a6b7db90a6397d69e3699da646f5fd74b3ed98c5
dromelvan <dromelvan@aland.net>
2020-06-16 22:14:57 +0300

    League table refactoring.

---
+++ src/components/TableContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="table-container">
+    <div class="table-header-container">
+      <slot name="header"></slot>
+    </div>
+    <v-divider/>
+    <div class="table-rows-container">
+      <slot></slot>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TableContainer'
+}
+</script>
+
+<style lang="scss" scoped>
+
+  .table-header-container,
+  .table-row {
+    display: flex;
+    padding: $d11-spacer 0;
+    line-height: 2.2em;
+
+    .main-column {
+      margin-right: auto;
+    }
+    .after-main-column {
+      margin-left: auto;
+    }
+
+    .emphasised {
+      font-weight: 600;
+    }
+  }
+
+  .table-header-container {
+    font-weight: 600;
+  }
+
+  .table-header-container > div,
+  .table-row > div {
+    min-width: 3.8em;
+    text-align: center;
+  }
+</style>
--- src/views/premier_leagues/PremierLeague.vue
+++ src/views/premier_leagues/PremierLeague.vue
@@ -36,61 +36,59 @@
     </header-section>
 
     <content-section>
-      <v-container class="premier-league-table-container">
-        <div class="premier-league-table" v-if="teamTableStats">
-          <div class="premier-league-table-stat-names">
-            <div class="premier-league-table-stat-name ranking">&nbsp;</div>
-            <div class="premier-league-table-stat-name team">Team</div>
-            <div class="premier-league-table-stat-name matches-played"><template v-if="smAndUp">Played</template><template v-else>Pl</template></div>
+      <table-container class="premier-league-table">
+
+        <template v-slot:header>
+          <div class="ranking">&nbsp;</div>
+          <div class="team">Team</div>
+          <div class="matches-played after-main-column"><template v-if="smAndUp">Played</template><template v-else>Pl</template></div>
+          <template v-if="smAndUp">
+            <div class="matches-won">Won</div>
+            <div class="matches-drawn">Drawn</div>
+            <div class="matches-lost">Lost</div>
+            <div class="goals-for"><template v-if="smAndDown">G+</template><template v-else>Goals+</template></div>
+            <div class="goals-against"><template v-if="smAndDown">G-</template><template v-else>Goals-</template></div>
+          </template>
+          <div class="goal-difference">GD</div>
+          <div class="points"><template v-if="smAndUp">Points</template><template v-else>Pts</template></div>
+        </template>
+
+        <div v-for="teamTableStat in teamTableStats" :key="teamTableStat.id">
+          <div class="table-row" v-bind:class="'ranking-' + teamTableStat.ranking">
+            <div class="ranking emphasised">{{ teamTableStat.ranking }}</div>
+            <div class="team"><team-image :version="'tiny'" :id="teamTableStat.team.id"/><template v-if="xs">{{ teamTableStat.team.shortName }}</template><template v-else>{{ teamTableStat.team.name }}</template></div>
+            <div class="table-up" v-if="teamTableStat.ranking < teamTableStat.previousRanking">
+              <v-tooltip top>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on"><table-up-icon/> +{{ teamTableStat.previousRanking - teamTableStat.ranking }}</span>
+                </template>
+                <span>Up {{ teamTableStat.previousRanking - teamTableStat.ranking }} positions from {{ teamTableStat.previousRanking }}</span>
+              </v-tooltip>
+            </div>
+            <div class="table-down" v-if="teamTableStat.ranking > teamTableStat.previousRanking">
+              <v-tooltip top>
+                <template v-slot:activator="{ on, attrs }">
+                  <span v-bind="attrs" v-on="on"><table-down-icon/> -{{ teamTableStat.ranking - teamTableStat.previousRanking }}</span>
+                </template>
+                <span>Down {{ teamTableStat.ranking - teamTableStat.previousRanking }} positions from {{ teamTableStat.previousRanking }}</span>
+              </v-tooltip>
+            </div>
+            <div class="winner emphasised" v-if="smAndUp && finished(premierLeague.seasonStatus) && teamTableStat.ranking === 1">Winner</div>
+            <div class="matches-played after-main-column">{{ teamTableStat.matchesPlayed }}</div>
             <template v-if="smAndUp">
-              <div class="premier-league-table-stat-name matches-won">Won</div>
-              <div class="premier-league-table-stat-name matches-drawn">Drawn</div>
-              <div class="premier-league-table-stat-name matches-lost">Lost</div>
-              <div class="premier-league-table-stat-name goals-for"><template v-if="smAndDown">G+</template><template v-else>Goals+</template></div>
-              <div class="premier-league-table-stat-name goals-against"><template v-if="smAndDown">G-</template><template v-else>Goals-</template></div>
+              <div class="matches-won">{{ teamTableStat.matchesWon }}</div>
+              <div class="matches-drawn">{{ teamTableStat.matchesDrawn }}</div>
+              <div class="matches-lost">{{ teamTableStat.matchesLost }}</div>
+              <div class="goals-for">{{ teamTableStat.goalsFor }}</div>
+              <div class="goals-against">{{ teamTableStat.goalsAgainst }}</div>
             </template>
-            <div class="premier-league-table-stat-name goal-difference">GD</div>
-            <div class="premier-league-table-stat-name points"><template v-if="smAndUp">Points</template><template v-else>Pts</template></div>
+            <div class="goal-difference"><template v-if="teamTableStat.goalDifference > 0">+</template>{{ teamTableStat.goalDifference }}</div>
+            <div class="points emphasised">{{ teamTableStat.points }}</div>
           </div>
           <v-divider/>
-
-          <div v-for="teamTableStat in teamTableStats" :key="teamTableStat.id">
-            <div class="premier-league-table-stats" v-bind:class="'ranking-' + teamTableStat.ranking">
-              <div class="premier-league-table-stat ranking">{{ teamTableStat.ranking }}</div>
-              <div><team-image :version="'tiny'" :id="teamTableStat.team.id"/></div>
-              <div class="premier-league-table-stat team"><template v-if="smAndUp">{{ teamTableStat.team.name }}</template><template v-else>{{ teamTableStat.team.shortName }}</template></div>
-              <div class="premier-league-table-stat table-up" v-if="smAndUp && teamTableStat.ranking < teamTableStat.previousRanking">
-                <v-tooltip top>
-                  <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on"><table-up-icon/> +{{ teamTableStat.previousRanking - teamTableStat.ranking }}</span>
-                  </template>
-                  <span>Up {{ teamTableStat.previousRanking - teamTableStat.ranking }} positions from {{ teamTableStat.previousRanking }}</span>
-                </v-tooltip>
-              </div>
-              <div class="premier-league-table-stat table-down" v-if="smAndUp && teamTableStat.ranking > teamTableStat.previousRanking">
-                <v-tooltip top>
-                  <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on"><table-down-icon/> -{{ teamTableStat.ranking - teamTableStat.previousRanking }}</span>
-                  </template>
-                  <span>Down {{ teamTableStat.ranking - teamTableStat.previousRanking }} positions from {{ teamTableStat.previousRanking }}</span>
-                </v-tooltip>
-              </div>
-              <div class="premier-league-table-stat winner" v-if="smAndUp && finished(premierLeague.seasonStatus) && teamTableStat.ranking === 1">Winner</div>
-              <div class="premier-league-table-stat matches-played">{{ teamTableStat.matchesPlayed }}</div>
-              <template v-if="smAndUp">
-                <div class="premier-league-table-stat matches-won">{{ teamTableStat.matchesWon }}</div>
-                <div class="premier-league-table-stat matches-drawn">{{ teamTableStat.matchesDrawn }}</div>
-                <div class="premier-league-table-stat matches-lost">{{ teamTableStat.matchesLost }}</div>
-                <div class="premier-league-table-stat goals-for">{{ teamTableStat.goalsFor }}</div>
-                <div class="premier-league-table-stat goals-against">{{ teamTableStat.goalsAgainst }}</div>
-              </template>
-              <div class="premier-league-table-stat goal-difference"><template v-if="teamTableStat.goalDifference > 0">+</template>{{ teamTableStat.goalDifference }}</div>
-              <div class="premier-league-table-stat points">{{ teamTableStat.points }}</div>
-            </div>
-            <v-divider/>
-          </div>
         </div>
-      </v-container>
+
+      </table-container>
     </content-section>
 
   </div>
@@ -114,7 +112,8 @@ export default {
     HeaderContentBackground: () => import('@/components/header/HeaderContentBackground'),
     ContentSection: () => import('@/components/ContentSection'),
     TableUpIcon: () => import('@/components/match-events/TableUpIcon'),
-    TableDownIcon: () => import('@/components/match-events/TableDownIcon')
+    TableDownIcon: () => import('@/components/match-events/TableDownIcon'),
+    TableContainer: () => import('@/components/TableContainer')
   },
   methods: {
     getPremierLeague: function () {
@@ -175,111 +174,44 @@ export default {
     }
   }
 
-  .premier-league-table-container {
-    .premier-league-table {
-      .premier-league-table-stat-names,
-      .premier-league-table-stats {
-        display: flex;
-        width: 100%;
-        padding-top: $d11-spacer;
-        padding-bottom: $d11-spacer;
-        padding-left: 0.5em;
-        padding-right: 1em;
-        line-height: 2.2em;
-      }
+  .premier-league-table {
+    .ranking {
+      min-width: 3em;
+    }
 
-      .premier-league-table-stat-names {
-        font-weight: 600;
-      }
-
-      .premier-league-table-stat-name,
-      .premier-league-table-stat {
-        padding-left: $d11-spacer;
-        padding-right: $d11-spacer;
-      }
+    .team {
+      text-align: left;
 
       .team-image {
         padding-right: $d11-spacer;
       }
+    }
 
-      .ranking {
-        font-weight: 600;
-        width: 2em;
-        text-align: center;
-      }
+    .ranking-1 {
+      background-color: $d11-blue;
+      color: white;
+    }
 
-      .winner,
-      .table-up,
-      .table-down {
-        font-weight: 600;
-      }
+    .ranking-2,
+    .ranking-3,
+    .ranking-4 {
+      background-color: $d11-blue-gray;
+      color: white;
+    }
 
-      .matches-played {
-        margin-left: auto;
-      }
-
-      .matches-played,
-      .matches-won,
-      .matches-drawn,
-      .matches-lost,
-      .goals-for,
-      .goals-against,
-      .goal-difference {
-        text-align: center;
-      }
-
-      .points {
-        font-weight: 600;
-        text-align: right;
-      }
-
-      .matches-played,
-      .matches-won,
-      .matches-drawn,
-      .matches-lost,
-      .goals-for,
-      .goals-against,
-      .goal-difference,
-      .points {
-        width: 3.8em;
-      }
-
-      .ranking-1 {
-        background-color: $d11-blue;
-        color: white;
-      }
-
-      .ranking-2,
-      .ranking-3,
-      .ranking-4 {
-        background-color: $d11-blue-gray;
-        color: white;
-      }
-
-      .ranking-18,
-      .ranking-19,
-      .ranking-20 {
-        background-color: $d11-gray;
-        color: white;
-      }
+    .ranking-18,
+    .ranking-19,
+    .ranking-20 {
+      background-color: $d11-gray;
+      color: white;
     }
   }
 
   .v-application-sm {
-    .premier-league-table-container {
-      .premier-league-table {
-        .premier-league-table-stat-names,
-        .premier-league-table-stats {
-
-          .goals-for,
-          .goals-against {
-            width: 2em;
-          }
-          .points,
-          .goal-difference {
-            width: 2.5em;
-          }
-        }
+    .premier-league-table {
+      .table-header-container > div,
+      .table-row > div {
+        min-width: 3.4em;
       }
     }
   }
@@ -289,16 +221,15 @@ export default {
       margin-top: 0px;
     }
 
-    .premier-league-table-container {
-      .premier-league-table {
-        .premier-league-table-stat-names,
-        .premier-league-table-stats {
-          .matches-played,
-          .goal-difference,
-          .points {
-            width: 2em;
-          }
-        }
+    .premier-league-table {
+      .table-header-container > div,
+      .table-row > div {
+        min-width: 2em;
+      }
+
+      .table-up,
+      .table-down {
+        padding-left: $d11-spacer;
       }
     }
   }
