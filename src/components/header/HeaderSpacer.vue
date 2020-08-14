425b09026d3962e5464f717f76d8636c850de448
dromelvan <dromelvan@aland.net>
2020-08-14 11:18:39 +0300

    Component for adding header space.

---
+++ src/components/header/HeaderSpacer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="header-spacer">
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSpacer'
+}
+</script>
+
+<style lang="scss" scoped>
+  .header-spacer {
+    padding: 0px;
+    height: $d11-background-picture-padding;
+  }
+</style>
